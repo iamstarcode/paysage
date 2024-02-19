2ed994e2a1e4161d537d382f3b42c22033610424
Bakare Abiola <iamstarcode@gmail.com>
2024-02-19 11:03:14 +0100

    chore: first commit

---
+++ supabase/seed.sql
@@ -0,0 +1,59 @@
+--PROFILES
+CREATE TABLE public.profiles (
+    user_id uuid not null references auth.users(id) on delete cascade,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    username VARCHAR(50)
+);
+
+CREATE INDEX idx_profile_user_id ON public.profiles(user_id);
+
+alter table public.profiles enable row level security;
+create policy "Public profiles are viewable only by authenticated users"
+    on public.profiles for select
+    to authenticated
+    using ( true );
+
+create policy "Users can update own profile."
+    on profiles for update
+    using ( auth.uid() = user_id );
+
+-- inserts a row into public.profiles
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = public
+as $$
+begin
+  insert into public.profiles (user_id)
+  values (new.user_id);
+
+  -- insert into public.wallets (user_id, balance)
+  -- values (new.id, 0);
+
+  return new;
+end;
+$$;
+
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+-->>PROFILES
+
+--WALLETS
+CREATE TABLE public.wallets (
+    id SERIAL PRIMARY KEY,
+    user_id uuid not null references auth.users(id), -- Reference to the user who owns the wallet
+    currency_code VARCHAR(3) NOT NULL, -- Currency code (e.g., USD, EUR, BTC)
+    balance NUMERIC(20, 2) DEFAULT 0, -- Balance of the currency in the wallet
+    UNIQUE(user_id, currency_code) -- Ensure each user has only one wallet per currency
+);
+
+alter table public.wallets enable row level security;
+
+create policy "Only Users can view own wallet."
+    on public.wallets for select
+    to authenticated
+    using ( auth.uid() = user_id );
+-->>WALLETS
