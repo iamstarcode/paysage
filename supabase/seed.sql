7e28feb793d019a08157bf07cae81f3a728a7b06
Bakare Abiola <iamstarcode@gmail.com>
2024-02-19 12:34:28 +0100

    chore: updated

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,12 +1,13 @@
---PROFILES
 CREATE TABLE public.profiles (
-    user_id uuid not null references auth.users(id) on delete cascade,
+    id uuid not null references auth.users on delete cascade,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    username VARCHAR(50)
+    username VARCHAR(50),
+
+    primary key (id)
 );
 
-CREATE INDEX idx_profile_user_id ON public.profiles(user_id);
+-- CREATE INDEX idx_profile_user_id ON public.profiles(user_id);
 
 alter table public.profiles enable row level security;
 create policy "Public profiles are viewable only by authenticated users"
@@ -15,8 +16,8 @@ create policy "Public profiles are viewable only by authenticated users"
     using ( true );
 
 create policy "Users can update own profile."
-    on profiles for update
-    using ( auth.uid() = user_id );
+    on public.profiles for update
+    using ( auth.uid() = id );
 
 -- inserts a row into public.profiles
 create function public.handle_new_user()
@@ -25,11 +26,8 @@ language plpgsql
 security definer set search_path = public
 as $$
 begin
-  insert into public.profiles (user_id)
-  values (new.user_id);
-
-  -- insert into public.wallets (user_id, balance)
-  -- values (new.id, 0);
+  insert into public.profiles (id)
+  values (new.id);
 
   return new;
 end;
@@ -39,15 +37,14 @@ $$;
 create trigger on_auth_user_created
   after insert on auth.users
   for each row execute procedure public.handle_new_user();
--->>PROFILES
 
---WALLETS
 CREATE TABLE public.wallets (
-    id SERIAL PRIMARY KEY,
-    user_id uuid not null references auth.users(id), -- Reference to the user who owns the wallet
+    id uuid not null references auth.users on delete cascade, -- Reference to the user who owns the wallet
     currency_code VARCHAR(3) NOT NULL, -- Currency code (e.g., USD, EUR, BTC)
     balance NUMERIC(20, 2) DEFAULT 0, -- Balance of the currency in the wallet
-    UNIQUE(user_id, currency_code) -- Ensure each user has only one wallet per currency
+    UNIQUE(id, currency_code), -- Ensure each user has only one wallet per currency
+
+    primary key (id)
 );
 
 alter table public.wallets enable row level security;
@@ -55,5 +52,5 @@ alter table public.wallets enable row level security;
 create policy "Only Users can view own wallet."
     on public.wallets for select
     to authenticated
-    using ( auth.uid() = user_id );
--->>WALLETS
+    using ( auth.uid() = id );
+
