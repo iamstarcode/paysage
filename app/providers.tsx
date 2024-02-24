eb39ae84ad4bfa20bd43e1e9572a321d4d0c189f
Bakare Abiola <iamstarcode@gmail.com>
2024-02-24 15:18:55 +0100

    feat: add query hooks

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,14 @@
 "use client"
 
 import { ThemeProvider } from "next-themes"
+import { SWRConfig } from "swr"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider defaultTheme="system" themes={["light", "cupckae", "dark"]}>
-      {children}
+      <SWRConfig value={{ revalidateIfStale: false, revalidateOnFocus: false }}>
+        {children}
+      </SWRConfig>
     </ThemeProvider>
   )
 }
