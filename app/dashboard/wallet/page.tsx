dfd85d7a3dac97a35a38bd628eea919fc20b7fda
StarCode <iamstarcode@gmail.com>
2024-05-12 23:15:08 +0100

    chore: update

--- app/dashboard/wallet/page.tsx
+++ app/dashboard/wallet/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
   await new Promise<void>((resolve) => {
     setTimeout(() => {
       resolve()
-    }, 500000)
+    }, 5000)
   })
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
